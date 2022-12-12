bb539eb721b7fd78dfe905711dcb746f8dc5d6fc
spockwall <abcdefg35216874@gmail.com>
2022-12-13 01:21:19 +0800

    keep try

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
 		// origin: "http://localhost:3000",
-		origin: "https://computer-network-phase2.onrender.com",
+		// origin: "https://computer-network-phase2.onrender.com",
 		origin: "*",
 		methods: ["GET", "POST"],
 	},
